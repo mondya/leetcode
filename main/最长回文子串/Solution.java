b454a763f410898c2e7ebedcadc77ceb98beebe3
xhh <xhh19990210@gmail.com>
2022-07-11 22:53:02 +0800

    :

---
+++ main/最长回文子串/Solution.java
@@ -0,0 +1,18 @@
+package 最长回文子串;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = "babad";
+        int b = s.lastIndexOf("b");
+        System.out.println(b);
+    }
+
+    public static String longestPalindrome(String s) {
+        return null;
+    }
+}
